4981a5dc0d951e6cacd78e75779de2a06dcedcee
troy <changtingweiforwork@gmail.com>
2018-01-24 12:12:22 +0800

    MinPerimeterRectangle

---
+++ Lesson10/MinPerimeterRectangle.php
@@ -0,0 +1,16 @@
+<?php
+
+function solution($N)
+{
+    $perimeter = 0;
+    $c = sqrt($N);
+    for ($i = 1; $i <= $c; $i++) {
+        if ($N % $i == 0) {
+            $perimeter = $i * 2 + ($N / $i) * 2;
+        }
+    }
+
+    return $perimeter;
+}
+
+var_dump(solution(30));
