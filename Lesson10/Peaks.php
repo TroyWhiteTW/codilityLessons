2c43177fe9c581fccb8f59c69737131ecf681ca4
troy <changtingweiforwork@gmail.com>
2018-03-01 17:30:06 +0800

    Peaks (Ted 版本)

--- Lesson10/Peaks.php
+++ Lesson10/Peaks.php
@@ -2,21 +2,48 @@
 
 function solution($A)
 {
+    error_reporting(0);
     $c = count($A);
+    $squrtC = sqrt($c);
     $peaks = [];
-    $peaks[] = false;
+    $countPeaks = 0;
+    $peaks[-1] = $countPeaks;
+    $peaks[0] = $countPeaks;
     for ($i = 1; $i < $c - 1; $i++) {
         if ($A[$i] > $A[$i - 1] && $A[$i] > $A[$i + 1]) {
-            $peaks[] = true;
+            $countPeaks++;
+            $peaks[$i] = $countPeaks;
         } else {
-            $peaks[] = false;
+            $peaks[$i] = $countPeaks;
         }
     }
-    $peaks[] = false;
+    $peaks[] = $countPeaks;
+    $divs = [1];
+    for ($i = 2; $i <= $squrtC; $i++) {
+        if ($c % $i == 0) {
+            $divs[] = $i;
+            $divs[] = $c / $i;
+        }
+    }
+    sort($divs);
 
-    $temp = [];
+    $answer = 0;
+    $countDivs = count($divs);
+    $flag = true;
+    for ($i = 0; $i < $countDivs; $i++) {
+        $interval = $c / $divs[$i];
+        for ($j = 1; $j <= $divs[$i]; $j++) {
+            if ($peaks[$j * $interval - 1] - $peaks[(($j - 1) * $interval) - 1] == 0) {
+                $flag = false;
+            }
+        }
+        if ($flag) {
+            $answer = $divs[$i];
+        }
+        $flag = true;
+    }
 
-    return $temp;
+    return $answer;
 }
 
 var_dump(solution([1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2]));
