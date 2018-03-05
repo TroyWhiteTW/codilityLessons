e9cf637b62fcca277a60bc6d0e6da8adce6b985a
troy <changtingweiforwork@gmail.com>
2018-03-05 14:32:30 +0800

    ChocolatesByNumbers 完成

--- Lesson12/ChocolatesByNumbers.php
+++ Lesson12/ChocolatesByNumbers.php
@@ -2,8 +2,28 @@
 
 function solution($N, $M)
 {
+    error_reporting(0);
 
-    return 5;
+//    if ($M == 1) {
+//        return $N;
+//    }
+//    $answer = 0;
+//    do {
+//        $answer++;
+//    } while (($M * $answer) % $N != 0);
+
+    $max = $N * $M;
+    for ($i = 1; $i <= $max; $i++) {
+        if (($N * $i) % $M == 0) {
+            return ($N * $i) / $M;
+        }
+        if (($M * $i) % $N == 0) {
+            return $i;
+        }
+    }
+    return $max;
 }
 
 var_dump(solution(10, 4));
+//var_dump(solution(12, 21));
+//var_dump(solution(2, 1));
