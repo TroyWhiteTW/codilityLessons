bccce821320751505c765047cea87e349f557c86
troy <changtingweiforwork@gmail.com>
2018-03-06 10:58:46 +0800

    ChocolatesByNumbers 加入 Ted 版本

--- Lesson12/ChocolatesByNumbers.php
+++ Lesson12/ChocolatesByNumbers.php
@@ -4,14 +4,6 @@ function solution($N, $M)
 {
     error_reporting(0);
 
-//    if ($M == 1) {
-//        return $N;
-//    }
-//    $answer = 0;
-//    do {
-//        $answer++;
-//    } while (($M * $answer) % $N != 0);
-
     $max = $N * $M;
     for ($i = 1; $i <= $max; $i++) {
         if (($N * $i) % $M == 0) {
@@ -22,6 +14,28 @@ function solution($N, $M)
         }
     }
     return $max;
+
+    // Ted 版本
+//    function getFactor($num)
+//    {
+//        $factorArray = [];
+//        for ($i = 2; $i <= $num; $i++) {
+//            if ($num % $i == 0) {
+//                $factorArray[] = $i;
+//                $num /= $i;
+//                $i = 1;
+//            }
+//        }
+//        return $factorArray;
+//    }
+//
+//    $factorM = getFactor($M);
+//    foreach ($factorM as $k => $v) {
+//        if ($N % $v == 0) {
+//            $N /= $v;
+//        }
+//    }
+//    return $N;
 }
 
 var_dump(solution(10, 4));
